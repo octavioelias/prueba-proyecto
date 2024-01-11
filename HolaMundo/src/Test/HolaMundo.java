809921fa94e9c299aadd623e08cc5cc1c123e4f7
20252477129 <20252477129@nb59531.gobiernocba.gov.ar>
2024-01-11 20:07:26 -0300

    mi primer commit

---
+++ HolaMundo/src/Test/HolaMundo.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+		
+		Persona hombre = new Persona();
+		Persona mujer = new Persona();
+		
+		int var = 5;
+	}
+}
